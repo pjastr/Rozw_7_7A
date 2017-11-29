079c7fa0d7f9f8f2db721ba43dd54dac9db9e2a6
Piotrek <@>
2017-11-29 13:42:32 +0100

    przeslanie plików

---
+++ Rozw_7_7A/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rozw_7_7A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pasazer p1 = new Pasazer("Jan");
+            Pasazer p2 = new Pasazer("Anna");
+            Pociag pociag = new Pociag();
+            pociag.DodajPasazera(p1);
+            pociag.DodajPasazera(p2);
+        }
+    }
+
+    class Pasazer
+    {
+        private string imie;
+
+        public Pasazer(string imie)
+        {
+            this.imie = imie;
+        }
+    }
+
+    class Pociag
+    {
+        private List<Pasazer> pasazerowie = new List<Pasazer>();
+
+        public Pociag()
+        {
+
+        }
+
+        public void DodajPasazera(Pasazer p)
+        {
+            pasazerowie.Add(p);
+        }
+
+
+    }
+}
